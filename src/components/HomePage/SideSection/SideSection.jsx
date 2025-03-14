7e8bda615dc2ee76d50e6aef99334f25f1dcf9f2
Kacper Szamszon <kacper.szamszon@gmail.com>
2025-03-14 11:09:51 +0100

    Dodanie panelu logowanie i rejestracji

--- src/components/HomePage/SideSection/SideSection.jsx
+++ src/components/HomePage/SideSection/SideSection.jsx
@@ -1,10 +1,67 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import style from './SideSection.module.css';
+import { FaArrowRight } from 'react-icons/fa6';
+
+const LogIn = () => {
+	return (
+		<div className={style.login}>
+			<h3>Zaloguj się</h3>
+			<form>
+				<div className={style.textbox}>
+					<input type='text' required />
+					<label>E-mail</label>
+				</div>
+				<div className={style.textbox}>
+					<input type='password' required />
+					<label>Hasło</label>
+				</div>
+				<button type='submit'>
+					<p>Zaloguj</p>
+					<FaArrowRight className={style.arrow} />
+				</button>
+			</form>
+			<span> Nie masz jeszcze konta?</span>
+			<Link to='/SignUp/'>
+				<span>Zarejestruj się</span>
+			</Link>
+		</div>
+	);
+};
+const SignUp = () => {
+	return (
+		<div className={style.login}>
+			<h3>Rejestracja</h3>
+			<form>
+				<div className={style.textbox}>
+					<input type='text' required />
+					<label>E-mail</label>
+				</div>
+				<div className={style.textbox}>
+					<input type='password' required />
+					<label>Hasło</label>
+				</div>
+				<button type='submit'>
+					<p>Zaloguj</p>
+					<FaArrowRight className={style.arrow} />
+				</button>
+			</form>
+			<span> Masz już konto?</span>
+			<Link to='/'>
+				<span>Zaloguj się</span>
+			</Link>
+		</div>
+	);
+};
 
 export const SideSection = () => {
-	return <div className={style.container}>
-    
-    <input/>
-    <input/>
-  </div>;
+	return (
+		<div className={style.container}>
+			<Routes>
+				<Route path='/' element={<LogIn />} />
+				<Route path='/SignUp' element={<SignUp />} />
+			</Routes>
+		</div>
+	);
 };
